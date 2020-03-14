62552f60ae6d1625547ec23a9e2d695c38307380
Idegart <degtyarev_artem@mail.ru>
2020-03-14 12:11:56 +0300

    Initial commit

---
+++ webpack.mix.js
@@ -0,0 +1,6 @@
+const mix = require('laravel-mix');
+
+mix
+    .webpackConfig(require('./webpack.config'))
+    .js('resources/js/app.js', 'public/dist/js')
+    .sass('resources/sass/app.scss', 'public/dist/css');
